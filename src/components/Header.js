fe1057d7b6cc12131b43fa4df923af25c5eb5249
KareeMagdyy <karim67899@gmail.com>
2022-05-06 20:33:46 +0200

    changing Header Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ function Header() {
       <div className='container'>
         <img src='img/troll-face.png' alt='logo' />
         <h1>Meme Generator</h1>
-        <p>React Course - Project 3</p>
       </div>
     </header>
   );
